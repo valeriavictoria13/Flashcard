a5d49d731e733c6110fdb01907c3e8ceb18a73bb
valeriavictoria13 <v.leon@escola.pr.gov.br>
2025-06-18 18:16:53 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,17 +1,16 @@
 criaCartao(
-    'categoria',
-    pergunta,
-    resposta
+    'Cicatrização Natural',
+    'Como fazer unguento pra feridas leves?',
+    'Mistura-se babosa, mel e azeite morno. A aplicação ajuda na cicatrização e hidratação da pele.'
+
+criaCartao(
+    'Fitoterapia Digestiva',
+    'Que planta alivia cólicas e gases?',
+    'Camomila com erva-doce, por suas propriedades calmantes e digestivas.'
 )
 
 criaCartao(
-    categoria,
-    pergunta,
-    resposta
-)
-
-criaCartao(
-    categoria,
-    pergunta,
-    resposta
+    'Banhos Terapêuticos',
+    'Que elementos compõem um banho relaxante oriental?',
+    'Ervas como chá verde, cipreste e folhas de bambu, usadas para purificação e relaxamento'
 )
