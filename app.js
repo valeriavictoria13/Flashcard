adadd1167710113bf232ea26dda235c9c4df9f73
valeriavictoria13 <v.leon@escola.pr.gov.br>
2025-06-18 19:35:20 -0300

    .

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ function criaCartao(categoria, pergunta, resposta) {
     cartao.className = 'cartao'
 
     cartao.innerHTML = `
-     <div class="cartao__conteudo">
-                    <h3>${categoria}</h3>
-                    <div class="cartao__conteudo__pergunta">
-                        <p>Que planta contém alcaloides que causam paralisia?</p>
-                        </div>
-                        <div class="cartao__conteudo__resposta">
-                            <p>Cicuta. Afeta o sistema nervoso e paralisa o corpo.</p>
-                        </div>
-                    </div>
-                
-    `
+<div class="cartao__conteudo">
+<h3>${categoria}</h3>
+<div class="cartao__conteudo__pergunta">
+        <p>${pergunta}</p>
+</div>
+<div class="cartao__conteudo__resposta">
+        <p>${resposta}</p>
+</div>
+</div>
+`
     container.appendChild(cartao)
 }
