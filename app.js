1db3bf95b2759258fc8ff7bc42245f034d6ab127
valeriavictoria13 <v.leon@escola.pr.gov.br>
2025-06-18 19:28:04 -0300

    .

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function criaCartao(categoria, pergunta, resposta) {
 
     cartao.innerHTML = `
      <div class="cartao__conteudo">
-                    <h3>Venenos</h3>
+                    <h3>${categoria}</h3>
                     <div class="cartao__conteudo__pergunta">
                         <p>Que planta contém alcaloides que causam paralisia?</p>
                         </div>
